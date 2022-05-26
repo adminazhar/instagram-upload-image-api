7ab97f2101c69d18224afbf49136f75be15f37d4
Azhar Khan <admin@azhark.com>
2022-05-26 22:38:17 +0530

    initial approach

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # instagram-upload-image-api
-A PHP API to upload images with hashtag to Instagram using Instagram Graph API.
+A PHP API to upload images with location, hashtag, caption to Instagram using Instagram Graph API.
+
+
+Access Tokens
+- Requires Access Tokes, and if possible never expire tokens
+
+Permissions
+-ads_management
+-business_management
+-instagram_basic
+-instagram_content_publish
+-pages_read_engagement
+
+Public Server
+- Media should be already hosted because Insgram Graph API uses cURL to get the image.
+
+
+Rate Limit
+Instagram accounts are limited to 25 API-published posts within a 24 hour moving period. Carousels count as a single post. This limit is enforced on the POST /{ig-user-id}/media_publish endpoint when attempting to publish a media container. We recommend that your app also enforce the publishing rate limit, especially if your app allows app users to schedule posts to be published in the future.
+
+
+Endpoints
+The API consists of the following endpoints. Refer to each endpoint's reference document for usage requirements.
+
+-POST /{ig-user-id}/media — upload media and create media containers.
+-POST /{ig-user-id}/media_publish — publish uploaded media using their media containers.
+-GET /{ig-container-id}?fields=status_code — check media container publishing eligibility and status.
+-GET /{ig-user-id}/content_publishing_limit — check app user's current publishing rate limit usage.
--- index.php
+++ index.php
@@ -1,2 +1,44 @@
 <?php
-echo "Hello Instagram";
+session_start();
+require_once "../automated-articles/library/functions/CallAPI.php";
+
+$access_token ="EAAo4ZC0ZBHudoBAJZAAEGgpwJtusj7HLBvAoVpi1E9fFqCEQmKZANvaVxuByleZBLE3vUvtf2GBzVJBaujn4maP8GPMkoi2ZCLMtqyMdlE4moqto1XJhJ19wjZB2XjEZBZBZBF3NdcKQIVdI7tKUUvTG3Cs2iJZAx7ZC6zZBmxJ9UZAIZA63WYBIPo5ZApK8";
+$client_id = "";
+$client_secret = "17841400008460056";
+$fb_page_name = "Dumyard2";
+function get_user_pages($page_name, $access_token) {
+    // Get the User's Pages
+    $get_user_pages = CallAPI("GET", "https://graph.facebook.com/v14.0/me/accounts?access_token=$access_token");
+    $get_user_pages = $get_user_pages['data'];
+    foreach($get_user_pages as $page) {
+        // Match the page with the page needed
+        if ($page['name'] == $fb_page_name) {
+            return $page['id'];
+        }
+    }
+    return FALSE;
+}
+
+// Get the Page's Instagram Business Account that's connected to the Facebook Page.
+function get_instagram_user_id(){
+
+}
+
+$page_id = get_user_pages($fb_page_name, $access_token);
+exit;
+
+$pageid = "17841400008460056";
+$page_id = "";
+
+$image_url = "https://nixby.in/wp-content/uploads/igtest.jpg";
+$caption = "%23HelloTest";
+// upload media and create media containers
+
+$container = "https://graph.facebook.com/v14.0/" . $pageid . "/media?image_url=" . $image_url ."&caption=" . $caption; 
+$get_ig_container_id = CallAPI("POST", $container);
+
+var_dump($get_ig_container_id);
+
+$_SESSION['id'] = $get_ig_container_id['id'];
+
+//  publish uploaded media using their media containers.
